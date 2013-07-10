b46e5d0d96219a527de66e680992ac6fc0a3efdb
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-07-09 21:19:52 -0400

    completed count.js

--- app/count.js
+++ app/count.js
@@ -3,6 +3,28 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var intervalId;
+      var self = this;
+
+      var startCounter = function() {
+        console.log(start);
+        intervalId = setInterval(increment, 100);
+      };
+
+      var increment = function() {
+        start++;
+        console.log(start);
+
+        if (start === end) {
+          self.cancel();
+        }
+      };
+
+      this.cancel = function() {
+        clearInterval(intervalId);
+      };
+
+      startCounter();
     }
   };
 });
